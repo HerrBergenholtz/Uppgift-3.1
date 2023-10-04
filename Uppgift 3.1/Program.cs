8102e2a32e1fdaab1188eb62265db6fe98676acd
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-04 11:06:20 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Tyvärr så är du för ung för att delta");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Tyvärr så är du för gammal för att delta i tävlingen");
+            }
+            else if (ålder > 16 && ålder < 19) {
+                Console.WriteLine("Grattis! Du får delta.");
+            }
+        }
+    }
+}
